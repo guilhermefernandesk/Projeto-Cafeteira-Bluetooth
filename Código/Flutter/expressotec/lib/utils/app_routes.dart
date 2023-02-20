51ba2e5d36beddbaf0c42ab7e9f294f1bf324707
Guilherme Fernandes <guilhermefernandes1937g@gmail.com>
2023-02-20 12:46:46 -0300

    Delete directory

--- Código/Flutter/expressotec/lib/utils/app_routes.dart
+++
@@ -1,7 +0,0 @@
-class AppRoutes {
-  static const authOrHome = '/';
-  static const bluetooth = '/bluetooth';
-  static const mymachine = '/mymachine';
-  static const capsulas = '/capsulas';
-  static const prepNow = '/prepNow';
-}

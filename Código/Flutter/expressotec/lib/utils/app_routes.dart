c0b4b5f300ed908b182d42f192e41777b684b144
Guilherme Fernandes <guilhermefernandes1937g@gmail.com>
2023-02-20 13:22:21 -0300

    Upload directory

---
+++ Código/Flutter/expressotec/lib/utils/app_routes.dart
@@ -0,0 +1,7 @@
+class AppRoutes {
+  static const authOrHome = '/';
+  static const bluetooth = '/bluetooth';
+  static const mymachine = '/mymachine';
+  static const capsulas = '/capsulas';
+  static const prepNow = '/prepNow';
+}
